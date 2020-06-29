783c00c72e6c063adc19ded461b689874725283c
bartcapote <gauclee@gmail.com>
2020-06-29 23:53:48 +0200

    Merge

--- static/js/game.js
+++ static/js/game.js
@@ -1,7 +1,21 @@
+//TEMPORARY GLOBAL DATA
+let cellsCount = 8;
+const game_field = document.querySelector('.game_field');
+
 initGame();
 
 function initGame() {
+    initLevel(cellsCount)
+}
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
-
+function initLevel(cellsCount) {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    let i;
+    const percentRandomizer = (distance) => ((Math.random() * distance) | 0) + '%';
+    for (i = 0; i < cellsCount; i++) {
+        cell.style.left = percentRandomizer(10);
+        cell.style.top = percentRandomizer(75);
+        game_field.appendChild(cell.cloneNode());
+    }
 }
