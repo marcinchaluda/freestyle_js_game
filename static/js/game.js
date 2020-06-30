ea690aca56c501e41bbde64b4e1c3d4d6eff87e6
bartcapote <gauclee@gmail.com>
2020-06-30 18:11:11 +0200

    Final Refactor

--- static/js/game.js
+++ static/js/game.js
@@ -3,7 +3,7 @@ NUMBER_OF_CELLS = 12;
 const VIRUSES = [];
 FIELD_WIDTH = 768;
 FIELD_HEIGHT = 250;
-
+FIELD_RATIO = 1.778
 
 const cellHandlers = {
     dragStart: function (e) {
@@ -17,7 +17,6 @@ const cellHandlers = {
     },
     dragEnter : function (e) {
         e.preventDefault();
-        e.target.classList.add(/* state change of drop target class */);
         console.log('dragenter');
     },
     dragOver: function (e) {
@@ -48,7 +47,6 @@ function initGame() {
 }
 
 function generateCells () {
-    const percentRandomizer = (distance) => ((Math.random() * distance) | 0) + '%';
     for (let i = 0; i < NUMBER_OF_CELLS; i++) {
         let cell = {
             left: Math.floor(Math.random() * FIELD_HEIGHT),
@@ -72,11 +70,11 @@ function insertVirusesOnGameField () {
         cellContainer.style.top = `${VIRUSES[i].top}px`;
         cellContainer.style.width = `${CELL_SIZE[Math.floor(Math.random() * CELL_SIZE.length)]}%`;
         cellContainer.style.height = Number(cellContainer.style.width.replace('%', ''))
-            * 1.778 + '%';
+            * FIELD_RATIO + '%';
         addCellListeners(cellContainer);
         distanceFromStartPosition += cellContainer.style.width;
-        // Add test content to first cell
-        if (i===0) cellContainer.textContent='50';
+        // Add test content to the first cell
+        (i===0) ? cellContainer.textContent = '50' : cellContainer.innerHTML = '&nbsp;';
         document.querySelector('.game_field').appendChild(cellContainer);
     }
 
