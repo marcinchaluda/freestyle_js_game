81af8d2c1b4e721baa52b4aa20fcfb3b7bcda1e4
bartcapote <gauclee@gmail.com>
2020-07-01 19:05:16 +0200

    More than 1 cell pass overlap check

--- static/js/game.js
+++ static/js/game.js
@@ -1,10 +1,10 @@
-CELL_SIZE = [6, 9, 10, 11];
-NUMBER_OF_CELLS = 12;
+CELL_SIZE = [5, 7, 9, 11];
+NUMBER_OF_CELLS = 25;
 const VIRUSES = [];
 FIELD_WIDTH = 768;
-FIELD_HEIGHT = 280;
-REQIURED_DISTANCE = (100/2);
+FIELD_HEIGHT = 432;
 FIELD_RATIO = 1.778;
+IMAGE_WIDTH = 3000;
 const PLAYER_COLOR = localStorage.getItem("playerColor"); //Variable with selected color by player
 
 const cellHandlers = {
@@ -19,7 +19,6 @@ const cellHandlers = {
     },
     dragEnter : function (e) {
         e.preventDefault();
-        e.target.classList.add(/* state change of drop target class */);
         console.log('dragenter');
     },
     dragOver: function (e) {
@@ -33,8 +32,10 @@ const cellHandlers = {
         e.preventDefault();
         let draggedElement = document.getElementById(e.dataTransfer.getData('text/elementid'));
         if (!(e.target).isSameNode(draggedElement)) {
-            const population = e.dataTransfer.getData('text/plain');
-            e.target.textContent = population;
+            const population = Number(e.dataTransfer.getData('text/plain'));
+            // add 35% population to target and take 38% from source (lose 3%):
+            e.target.textContent = (Number(e.target.textContent) + population * 0.35 | 0).toString();
+            draggedElement.textContent = (population * 0.62 | 0).toString();
             console.log('drop');
         }
     }
@@ -50,64 +51,93 @@ function initGame() {
 }
 
 function generateCells () {
+    // let widthOfCell = 0;
+    // let heightOfCell = 0;
     for (let i = 0; i < NUMBER_OF_CELLS; i++) {
-    // while(VIRUSES.length < NUMBER_OF_CELLS) {
+        const cellSize = CELL_SIZE[Math.floor(Math.random() * CELL_SIZE.length)];
         let cell = {
-            left: Math.floor(Math.random() * (FIELD_WIDTH/1.5)),
+            left: Math.floor(Math.random() * (FIELD_WIDTH)),
             top: Math.floor(Math.random() * (FIELD_HEIGHT)),
             avatar: 'virus_default',
-            width: CELL_SIZE[Math.floor(Math.random() * CELL_SIZE.length)],
+            width: cellSize,
         };
-        calculateDistance(cell);
+        if (!checkRange(cell)) {
+            VIRUSES.push(cell);
+        }
     }
 }
 
-function calculateDistance(currentCell) {
-    if (VIRUSES.length === 0) {
-        VIRUSES.push(currentCell);
-        return;
+ function checkRange(currentCell) {
+    let overlapping = false;
+     if (VIRUSES.length === 0) {
+        return overlapping;
     }
-     let overlapping = false;
-     for (let j = 0; j < VIRUSES.length; j++){
-            let otherCell = VIRUSES[j];
-            // let distance = Math.floor(Math.sqrt(Math.pow(Math.abs(currentCell.left - otherCell.left), 2) +
-            // Math.pow(Math.abs(currentCell.top - otherCell.top), 2)));
-            //
-            //  if (distance <= Math.abs(currentCell.left - otherCell.left) || distance <= Math.abs(currentCell.top - otherCell.top)){
-            //      overlapping = true;
-            //      break;
-            //  }
-             if (!(currentCell.left + currentCell.width < otherCell.left ||
-                 currentCell.left > otherCell.left + otherCell.width ||
-                 currentCell.top + currentCell.width < otherCell.top ||
-                 currentCell.top > otherCell.top + otherCell.width)) {
-                overlapping = true;
-                break;
-             }
+     console.log(VIRUSES)
+    for (let otherCell of VIRUSES) {
+        const width = (currentCell.width/100) * FIELD_WIDTH;
+        const otherCellWidth = (otherCell.width/100) * FIELD_WIDTH;
+        console.log(width)
+        console.log(otherCellWidth)
+         if (!(currentCell.left + width < otherCell.left ||
+             currentCell.left > otherCell.left + otherCellWidth||
+             currentCell.top + width < otherCell.top ||
+             currentCell.top > otherCell.top + otherCellWidth)) {
+            overlapping = true;
+            break;
          }
-     if (!overlapping) {
-         VIRUSES.push(currentCell);
      }
+     return overlapping;
 }
+// function calculateDistance(currentCell) {
+//     if (VIRUSES.length === 0) {
+//         VIRUSES.push(currentCell);
+//         // return;
+//     }
+//      let overlapping = false;
+//      for (let j = 0; j < VIRUSES.length; j++){
+//             let otherCell = VIRUSES[j];
+//             const width = CELL_SIZE[currentCell.width]/100 * 3000;
+//             const otherCellWidth = CELL_SIZE[otherCell.width]/100 * 3000;
+//             // let distance = Math.floor(Math.sqrt(Math.pow(Math.abs(currentCell.left - otherCell.left), 2) +
+//             // Math.pow(Math.abs(currentCell.top - otherCell.top), 2)));
+//             //
+//             //  if (distance <= Math.abs(currentCell.left - otherCell.left) || distance <= Math.abs(currentCell.top - otherCell.top)){
+//             //      overlapping = true;
+//             //      break;
+//             //  }
+//              if (!(currentCell.left + width < otherCell.left ||
+//                  currentCell.left > otherCell.left + otherCellWidth||
+//                  currentCell.top + width * FIELD_RATIO< otherCell.top ||
+//                  currentCell.top > otherCell.top + otherCellWidth * FIELD_RATIO)) {
+//                 overlapping = true;
+//                 break;
+//              }
+//          }
+//      if (!overlapping) {
+//          VIRUSES.push(currentCell);
+//      }
+// }
 
 function insertVirusesOnGameField () {
-    let distanceFromStartPosition = 0;
+
     for (let i = 0; i < VIRUSES.length; i++) {
         const cellContainer = document.createElement('div');
         cellContainer.style.background = `url('static/img/${VIRUSES[i].avatar}.png')`;
         cellContainer.style.backgroundSize = 'cover';
         cellContainer.classList.add('cell');
-        cellContainer.style.left = `${VIRUSES[i].left - distanceFromStartPosition}px`;
+        cellContainer.style.left = `${VIRUSES[i].left}px`;
         cellContainer.style.top = `${VIRUSES[i].top}px`;
         cellContainer.style.width = `${VIRUSES[i].width}%`;
         cellContainer.style.height = Number(cellContainer.style.width.replace('%', ''))
             * FIELD_RATIO + '%';
         addCellListeners(cellContainer);
-        distanceFromStartPosition += cellContainer.style.width;
-        // Add test content to first cell
-        if (i===0) cellContainer.textContent='50';
+
+        // Add test content to the first cell
+        (i===0) ? cellContainer.textContent = '50' : cellContainer.innerHTML = '&nbsp;';
+        grow(cellContainer);
         document.querySelector('.game_field').appendChild(cellContainer);
     }
+
 }
 
 function addCellListeners(element) {
@@ -120,3 +150,16 @@ function addCellListeners(element) {
     element.addEventListener('drop', cellHandlers.drop);
 }
 
+function grow(cell) {
+    const breed = function () {
+        let population = Number(cell.textContent);
+        const populationCap = Number(cell.style.width.replace('%', '')) * 10 | 0;
+        if (population > 0) {
+            let brood = (population * (Math.random() * 3 + 3 | 0) * 0.01) | 0; // generates integer 3-5% population
+            population += (brood > 0) ? brood : 1;
+            (cell.textContent <= populationCap) ?
+                cell.textContent = population.toString() : cell.textContent = populationCap;
+        }
+    }
+    setInterval(breed, 300 + (Math.random() * 300) | 0);
+}
